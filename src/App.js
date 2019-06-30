a1a1097d166b8d3c548045d09723fc19c72227c7
ccowden <cecowden@utexas.edu>
2019-06-30 15:04:32 -0500

    Added header bar with logo at top. Added images for page links.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,55 @@
-import React from 'react';
-import {
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
+import React from 'react'
+import { Route, NavLink, HashRouter } from 'react-router-dom'
 
-import logo from './logo.svg';
-import './App.css';
-import Home from './Home.js';
-import About from './About.js';
-import Contact from './Contact.js';
-import Gallery from './Gallery.js';
+import logo from './assets/cookiebarne-logo.png'
+import pineapple from './assets/pineapple.jpg'
+import birthday from './assets/birthday.jpg'
+import chick from './assets/chick.jpg'
+import './App.css'
+import Home from './Home.js'
+import About from './About.js'
+import Contact from './Contact.js'
+import Gallery from './Gallery.js'
 
-function App() {
+function App () {
   return (
-    <div className="App">
+    <div className='App'>
       <HashRouter>
-        <h1 className="headerImg">
-          <div>The Cookie Barne</div>
-          <div className="wrapper">
-          <ul className="header">
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/about">About</NavLink></li>
-            <li><NavLink to="/gallery">Gallery</NavLink></li>
-            <li><NavLink to="/contact">Contact Us</NavLink></li>
-          </ul>
-          </div>
+        <div className='title-nav'>
+          <NavLink to='/'>
+            <img src={logo} alt={'test'} className='cookiebarne-logo' />
+          </NavLink>
+        </div>
+        <h1 className='headerImg'>
+          <div className='wrapper' />
         </h1>
-        
-        <div className="content">
-          <Route exact path="/" component={Home}/>
-          <Route path="/about" component={About}/>
-          <Route path="/gallery" component={Gallery}/>
-          <Route path="/contact" component={Contact}/>
+        <ul className='homepage-three-boxes-wrap clearfix'>
+          <li className='box first'>
+            <NavLink to='/about'>
+              <img src={pineapple} alt={'About Us'} class='navTile' />
+            </NavLink>
+          </li>
+          <li className='box first'>
+            <NavLink to='/gallery'>
+              <img src={chick} alt={'Gallery'} class='navTile' />
+            </NavLink>
+          </li>
+          <li className='box first'>
+            <NavLink to='/contact'>
+              <img src={birthday} alt={'Place an Order'} class='navTile' />
+            </NavLink>
+          </li>
+        </ul>
+
+        <div className='content'>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/gallery' component={Gallery} />
+          <Route path='/contact' component={Contact} />
         </div>
       </HashRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
