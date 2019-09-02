e8adac0b6e7a531fc67396f05451c34e2022adde
ccowden <cecowden@utexas.edu>
2019-09-02 14:52:16 -0500

    Checking in build folder, for Azure deployment

---
+++ build/precache-manifest.05d353f01255a90eeeff5a66242844bf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4806e18b0b8f87c6dd82142dbfa17b40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68c8425707df1757137e",
+    "url": "/static/css/main.b222e9a2.chunk.css"
+  },
+  {
+    "revision": "bb55cd4801b1eed7a328",
+    "url": "/static/js/2.5d9b5dde.chunk.js"
+  },
+  {
+    "revision": "68c8425707df1757137e",
+    "url": "/static/js/main.7d3d8361.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "49e491d01c3bbdaf856091c4b8e67e75",
+    "url": "/static/media/barn.49e491d0.jpg"
+  },
+  {
+    "revision": "3ff6fece0e6778ffc0e817324a3ca508",
+    "url": "/static/media/birthday.3ff6fece.jpg"
+  },
+  {
+    "revision": "a6936a83007c913e8c3f66e5a619be50",
+    "url": "/static/media/chick.a6936a83.jpg"
+  },
+  {
+    "revision": "b2e01fe29cbc7a081085cb5b284904f0",
+    "url": "/static/media/pineapple.b2e01fe2.jpg"
+  }
+]);
