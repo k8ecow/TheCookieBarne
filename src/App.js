cd8189329541ebf9c6e0b0e752ac6aec5cef368a
ccowden <catie.cowden@gmail.com>
2020-03-20 21:12:04 -0700

    Refactored from SPA to regular routing

--- src/App.js
+++ src/App.js
@@ -1,52 +1,55 @@
 import React from 'react';
 import './styles/App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import About from './components/About.js';
 import Gallery from './components/Gallery.js';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import NavItem from 'react-bootstrap/NavItem';
+import Occasions from './components/Occasions.js';
 import PlaceOrder from './components/PlaceOrder.js';
-import ScrollableAnchor from 'react-scrollable-anchor';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <Navbar collapseOnSelect sticky="top" bg="light" expand="lg">
-        <Navbar.Brand href="\" className="shadow-font">
-          The Cookie Barne
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#about" className="nav-links">
-              About Us
-            </Nav.Link>
-            <Nav.Link href="#gallery" className="nav-links">
-              Gallery
-            </Nav.Link>
-            <Nav.Link href="#order" className="nav-links">
-              Place an Order
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <h1 className="headerImg">
-        <div className="wrapper" />
-      </h1>
-      <br />
-      <br />
-      <ScrollableAnchor id={'about'}>
-        <About />
-      </ScrollableAnchor>
-      <br />
-      <br />
-      <ScrollableAnchor id={'gallery'}>
-        <Gallery />
-      </ScrollableAnchor>
-      <br />
-      <br />
-      <ScrollableAnchor id={'order'}>
-        <PlaceOrder />
-      </ScrollableAnchor>
+      <Router history={About}>
+        <Navbar bg="light" collapseOnSelect expand="lg">
+          <Navbar.Brand href="\" className="shadow-font">
+            The Cookie Barne
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav>
+              <LinkContainer to="/" className="nav-links">
+                <NavItem>Home</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/about" className="nav-links">
+                <NavItem>About Us</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/gallery" className="nav-links">
+                <NavItem>Gallery</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/occasions" className="nav-links">
+                <NavItem>Occasions</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/order" className="nav-links">
+                <NavItem>Place Order</NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <h1 className="headerImg">
+          <div className="wrapper" />
+        </h1>
+        <Switch>
+          <Route path="/" exact component={About} />
+          <Route path="/about" exact component={About} />
+          <Route path="/gallery" exact component={Gallery} />
+          <Route path="/occasions" exact component={Occasions} />
+          <Route path="/order" exact component={PlaceOrder} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Occasions.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../styles/About.css';
+
+class Occasions extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Occasions</h2>
+        <p className="occasions">Coming soon</p>
+      </div>
+    );
+  }
+}
+
+export default Occasions;
